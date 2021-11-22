76c4f80c6bf4e3d4625dc00761c7587f6b19a692
Luis D. Torres <luis.torres@recode.ai>
2021-11-22 17:25:41 -0300

    Packages

--- Code/01_load_packages.R
+++ Code/01_load_packages.R
@@ -8,7 +8,6 @@
 
 #### packages needed
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(corrr)) install.packages("corrr", repos = "http://cran.us.r-project.org")
 if(!require(Hmisc)) install.packages("Hmisc", repos = "http://cran.us.r-project.org")
 if(!require(sjlabelled)) install.packages("sjlabelled", repos = "http://cran.us.r-project.org")
 if(!require(sjmisc)) install.packages("sjmisc", repos = "http://cran.us.r-project.org")
@@ -19,10 +18,9 @@ if(!require(semPlot)) install.packages("semPlot", repos = "http://cran.us.r-proj
 if(!require(haven)) install.packages("haven", repos = "http://cran.us.r-project.org")
 if(!require(psych)) install.packages("psych", repos = "http://cran.us.r-project.org")
 if(!require(xlsx)) install.packages("xlsx", repos = "http://cran.us.r-project.org")
-if(!require(tidySEM)) install.packages("tidySEM", repos = "http://cran.us.r-project.org")
 if(!require(kableExtra)) install.packages("kableExtra", repos = "http://cran.us.r-project.org")
-if(!require(tidygraph)) install.packages("tidygraph", repos = "http://cran.us.r-project.org")
-if(!require(ggraph)) install.packages("ggraph", repos = "http://cran.us.r-project.org")
+if(!require(e1071)) install.packages("e1071", repos = "http://cran.us.r-project.org")
+if(!require(gvlma)) install.packages("gvlma", repos = "http://cran.us.r-project.org")
 
 library(lavaan) # CFA and SEM
 library(semTools) # additional toold for CFA and SEM 
@@ -32,14 +30,13 @@ library(haven) # read SPSS, Stata, and SAS data
 library(tidyverse) # several tools for data manipulation
 library(Hmisc) # many functions useful for data analysis
 library(sjlabelled) # get_labels function
-library(corrr) # for correlations correlate()
 library(psych) # descriptives
 library(labelled) # working with labelled data
 library(flextable) # formatting tables 
 library(xlsx) # export data into excel
-library(tidySEM) # for SEM plotting
 library(kableExtra) # for SEM plotting
-library(tidygraph) 
-library(ggraph)
+library(gvlma) # assumptions checks
+library(e1071) # skewness
+
 
 # End load packages
